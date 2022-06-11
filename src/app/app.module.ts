e7e67e7c1fbfb47183499fed801d398e65bbe127
Tiago Lima <james.lima.devops@gmail.com>
2022-06-11 15:37:55 -0300

    Criado a main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './components/templates/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
