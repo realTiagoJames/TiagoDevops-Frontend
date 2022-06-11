b10abb876269595ab86357cd1c2aef3b255a5af0
Tiago Lima <james.lima.devops@gmail.com>
2022-06-11 16:26:40 -0300

    Criado Tarja Superior

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { MainComponent } from './components/templates/main/main.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
   {
     path:'',
     component: HomeComponent
