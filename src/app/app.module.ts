e073e1ef2d789dcab2568d6a7ee229b608d97475
Tiago Lima <james.lima.devops@gmail.com>
2022-06-11 22:13:26 -0300

    Criado servicos e removido texto da tarja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MainComponent } from './components/templates/main/main.component';
 import { HomeComponent } from './views/home/home.component';
 import { MenuComponent } from './components/templates/menu/menu.component';
 import {MatMenuModule} from '@angular/material/menu';
+import { ServicosComponent } from './views/views-main/servicos/servicos.component';
 
 
 
@@ -19,6 +20,7 @@ import {MatMenuModule} from '@angular/material/menu';
     HomeComponent,
     MainComponent,
     MenuComponent,
+    ServicosComponent,
   ],
   imports: [
     BrowserModule,
