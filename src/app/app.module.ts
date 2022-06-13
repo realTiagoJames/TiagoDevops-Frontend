1e4841d59e276a5d93d94897cff5f90df70140ff
Tiago Lima <james.lima.devops@gmail.com>
2022-06-13 16:44:23 -0300

    Imports em ordem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './components/templates/menu/menu.component';
-import { HomeComponent } from './views/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { CloudComponent } from './views/views-main/cloud/cloud.component';
 import { ContatoComponent } from './views/views-main/contato/contato.component';
 import { CriacaoSistemasComponent } from './views/views-main/criacao-sistemas/criacao-sistemas.component';
 import { CriacaoWebsitesComponent } from './views/views-main/criacao-websites/criacao-websites.component';
-import { ServicosComponent } from './views/views-main/servicos/servicos.component';
 import { FooterComponent } from './components/templates/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MenuComponent } from './components/templates/menu/menu.component';
+import { NgModule } from '@angular/core';
+import { ServicosComponent } from './views/views-main/servicos/servicos.component';
 
 
 
