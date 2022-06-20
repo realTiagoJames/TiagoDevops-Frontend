9d0522b67902bb8ee9bf9cb0e22a6afd710e8522
Tiago Lima <james.lima.devops@gmail.com>
2022-06-19 21:29:18 -0300

    Criado o Alert e desfixado o footer

--- src/app/components/templates/menu/menu.component.ts
+++ src/app/components/templates/menu/menu.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [
+{
+  type: 'dark',
+  message: 'Helcome to my Website! Consulte a disponibilidade de serviços na sua região. Leia mais',
+}
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,9 +19,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.reset();
+  }
 
   ngOnInit(): void {
+  }
+
+  alerts!: Alert[];
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+   reset() {
+  this.alerts = Array.from(ALERTS);
   }
 
 }
